71e7ebe8a425d35989ccb69b5df1ab4131a0102c
TM6AleksanderJunge <124243735+TM6AleksanderJunge@users.noreply.github.com>
2023-05-16 14:40:58 +0200

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -2,20 +2,20 @@ AZUREPAT=$AZUREPAT
 AZUSERNAME=$AZUSERNAME
 AZUSER_EMAIL=$AZUSER_EMAIL
 AZORG=$AZORG
-
+AZREPO=$AZREPO
 #git clone https://github.com/TM6AleksanderJunge/testing-sync-to-azure-repo/tree/syncing-branch
+
 #cd testing-sync-to-azure-repo
 
 rm -rf .git
 cd ..
-rm -rf AzureRepos-syncing
-
-GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/syncing-test/_git/AzureRepos-syncing"
+rm -rf $AZREPO
+GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/syncing-test/_git/$AZREPO"
 git clone $GIT_CMD_REPOSITORY
 
-cp -r testing-sync-to-azure-repo/* AzureRepos-syncing/
+cp -r testing-sync-to-azure-repo/* $AZREPO/
 
-cd AzureRepos-syncing
+cd $AZREPO
 
 git config --global user.email "$AZUSER_EMAIL"
 git config --global user.name "$AZUSERNAME"
