d8529ce4156e8b37703bbb957f8286f80c947104
TM6AleksanderJunge <124243735+TM6AleksanderJunge@users.noreply.github.com>
2023-05-30 10:39:42 +0200

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -3,24 +3,23 @@ AZUSERNAME=$AZUSERNAME
 AZUSER_EMAIL=$AZUSER_EMAIL
 AZORG=$AZORG
 AZREPO=$AZREPO
-#git clone https://github.com/TM6AleksanderJunge/testing-sync-to-azure-repo/tree/syncing-branch
-
-#cd testing-sync-to-azure-repo
 
 rm -rf .git
 cd ..
 rm -rf $AZREPO
-GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/syncing-test/_git/$AZREPO"
-git clone $GIT_CMD_REPOSITORY
+#GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/syncing-test/_git/$AZREPO"
+#git clone $GIT_CMD_REPOSITORY
 
-cp -r testing-sync-to-azure-repo/* $AZREPO/
+#cp -r testing-sync-to-azure-repo/* $AZREPO/
 
-cd $AZREPO
+#cd $AZREPO
 
-git config --global user.email "$AZUSER_EMAIL"
-git config --global user.name "$AZUSERNAME"
+#git config --global user.email "$AZUSER_EMAIL"
+#git config --global user.name "$AZUSERNAME"
+
+git remote set-url --add --push origin https://$AZUSERNAME:$AZPAT@dev.azure.com/$AZORG/syncing-test/_git/$AZREPO
 
 git add .
 git commit -m "sync from git to azure"
 
-git push
+git push origin syncing-branch:arrival-branch
